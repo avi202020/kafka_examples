ef1f3e5dd3faf6cdedee7a6cabd2bf2de3704c23
Josh Patterson <jpatterson@floe.tv>
2018-05-25 15:28:59 -0400

    updated pipe nodes in header

--- src/main/java/com/pattersonconsultingtn/kafka/examples/Pipe.java
+++ src/main/java/com/pattersonconsultingtn/kafka/examples/Pipe.java
@@ -9,6 +9,47 @@ import org.apache.kafka.streams.Topology;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
  
+ /**
+
+	If we follow the Apache Kafka Quickstart too closely, we end up with this issue for the pipe demo (doesnt have a key like the wordcount):
+
+	https://stackoverflow.com/questions/49098274/kafka-stream-get-corruptrecordexception
+
+	Updates:
+
+	(1) topic creation for input
+
+	bin/kafka-topics.sh --create \
+	    --zookeeper localhost:2181 \
+	    --replication-factor 1 \
+	    --partitions 1 \
+	    --topic streams-plaintext-input
+
+	(2) topic creation for output
+
+	bin/kafka-topics.sh --create \
+	--zookeeper localhost:2181 \
+	--replication-factor 1 \
+	--partitions 1 \
+	--topic streams-pipe-output
+
+
+	(3) kafka producer setup from console
+
+	bin/kafka-console-producer.sh --broker-list localhost:9092 --topic streams-plaintext-input
+
+	(4) kafka consumer setup from console
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+--topic streams-pipe-output \
+--from-beginning \
+--formatter kafka.tools.DefaultMessageFormatter \
+--property print.key=true \
+--property print.value=true \
+--property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+--property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+
+ */
 public class Pipe {
  
     public static void main(String[] args) throws Exception {
