4f365203dbfcab46936fe5ddcd6b2fe47f285231
Josh Patterson <jpatterson@floe.tv>
2018-07-10 10:44:27 -0400

    rebuilt the pom to work w confluent repo and get the pollution sensor producer class setup to dev on [more to go] but this compiles

--- src/main/java/com/pattersonconsultingtn/kafka/examples/Basic/Pipe.java
+++ src/main/java/com/pattersonconsultingtn/kafka/examples/Basic/Pipe.java
@@ -1,4 +1,4 @@
-package com.pattersonconsultingtn.kafka.examples;
+package com.pattersonconsultingtn.kafka.examples.Basic;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
