de4e9a73402dba5ed84a522edd86eff449332303
Josh Patterson <jpatterson@floe.tv>
2018-07-11 14:28:05 -0400

    this sensor collector works, the instructions show the avro data collected in the topic

--- src/main/java/com/pattersonconsultingtn/kafka/examples/PollutionDataTracker/PollutionSensorProducer.java
+++ src/main/java/com/pattersonconsultingtn/kafka/examples/PollutionDataTracker/PollutionSensorProducer.java
@@ -11,6 +11,61 @@ import java.util.Date;
 import java.util.Properties;
 import java.util.Random;
 
+/**
+
+Quick Start
+
+# Start Zookeeper. Since this is a long-running service, you should run it in its own terminal.
+$ ./bin/zookeeper-server-start ./etc/kafka/zookeeper.properties
+
+# Start Kafka, also in its own terminal.
+$ ./bin/kafka-server-start ./etc/kafka/server.properties
+
+# Start the Schema Registry, also in its own terminal.
+./bin/schema-registry-start ./etc/schema-registry/schema-registry.properties
+
+
+
+// Create topic in Kafka
+
+./bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 \
+                   --partitions 1 --topic pollution_sensor_reading
+
+
+// Run the producer from maven
+
+mvn exec:java -Dexec.mainClass="com.pattersonconsultingtn.kafka.examples.PollutionDataTracker.PollutionSensorProducer" \
+  -Dexec.args="10 http://localhost:8081"
+
+
+
+// check topic for entries
+./bin/kafka-avro-console-consumer --zookeeper localhost:2181 --topic pollution_sensor_reading --from-beginning
+
+Using the ConsoleConsumer with old consumer is deprecated and will be removed in a future major release. Consider using the new consumer by passing [bootstrap-server] instead of [zookeeper].
+{"timestamp":1531332977183,"sensor_reading":0.0}
+{"timestamp":1531332978318,"sensor_reading":1.0}
+{"timestamp":1531332978318,"sensor_reading":2.0}
+{"timestamp":1531332978319,"sensor_reading":3.0}
+{"timestamp":1531332978319,"sensor_reading":4.0}
+{"timestamp":1531332978319,"sensor_reading":5.0}
+{"timestamp":1531332978319,"sensor_reading":6.0}
+{"timestamp":1531332978319,"sensor_reading":7.0}
+{"timestamp":1531332978320,"sensor_reading":8.0}
+{"timestamp":1531332978320,"sensor_reading":9.0}
+{"timestamp":1531333171407,"sensor_reading":0.0}
+{"timestamp":1531333171816,"sensor_reading":1.0}
+{"timestamp":1531333171816,"sensor_reading":2.0}
+{"timestamp":1531333171816,"sensor_reading":3.0}
+{"timestamp":1531333171817,"sensor_reading":4.0}
+{"timestamp":1531333171817,"sensor_reading":5.0}
+{"timestamp":1531333171817,"sensor_reading":6.0}
+{"timestamp":1531333171817,"sensor_reading":7.0}
+{"timestamp":1531333171817,"sensor_reading":8.0}
+{"timestamp":1531333171817,"sensor_reading":9.0}
+
+
+*/
 public class PollutionSensorProducer {
 
   public static void main(String[] args){
@@ -29,31 +84,51 @@ public class PollutionSensorProducer {
     props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
     props.put("schema.registry.url", url);
 
-    String schemaString = "{\"namespace\": \"example.avro\", \"type\": \"record\", " +
-                           "\"name\": \"page_visit\"," +
+/*
+
+{
+  "namespace" : "com.pattersonconsulting.kafka.avro",
+  "type" : "record",
+  "name" : "Sensor",
+  "fields" : [
+    {"name":"timestamp","type":"long"},
+    {"name":"sensor_reading", "type":"float", "default":0.0} 
+  ]
+}
+
+
+*/
+
+    String topicName = "pollution_sensor_reading";
+    String topicKey = "sensor_0"; // sensorID
+
+    String schemaString = "{\"namespace\": \"example.avro\", " +
+                            "\"type\": \"record\", " +
+                           "\"name\": \"" + topicName + "\"," +
                            "\"fields\": [" +
-                            "{\"name\": \"time\", \"type\": \"long\"}," +
-                            "{\"name\": \"site\", \"type\": \"string\"}," +
-                            "{\"name\": \"ip\", \"type\": \"string\"}" +
+                            "{\"name\": \"timestamp\", \"type\": \"long\"}," +
+                            "{\"name\": \"sensor_reading\", \"type\": \"float\", \"default\":0.0 }" +
                            "]}";
+
     Producer<String, GenericRecord> producer = new KafkaProducer<String, GenericRecord>(props);
 
     Schema.Parser parser = new Schema.Parser();
-    Schema schema = parser.parse(schemaString);
+    Schema schema = parser.parse( schemaString );
 
     Random rnd = new Random();
+
     for (long nEvents = 0; nEvents < events; nEvents++) {
+
       long runtime = new Date().getTime();
-      String site = "www.example.com";
-      String ip = "192.168.2." + rnd.nextInt(255);
+      float readingVal = nEvents * 1.0f;
 
-      GenericRecord page_visit = new GenericData.Record(schema);
-      page_visit.put("time", runtime);
-      page_visit.put("site", site);
-      page_visit.put("ip", ip);
+      GenericRecord reading_record = new GenericData.Record(schema);
+      reading_record.put("timestamp", runtime);
+      reading_record.put("sensor_reading", readingVal);
+      //page_visit.put("ip", ip);
 
-      ProducerRecord<String, GenericRecord> data = new ProducerRecord<String, GenericRecord>(
-          "page_visits", ip, page_visit);
+      // ##############                                                                      topic, key, value
+      ProducerRecord<String, GenericRecord> data = new ProducerRecord<String, GenericRecord>( topicName, topicKey, reading_record );
       producer.send(data);
     }
 
